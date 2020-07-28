067b24e4d55cc94efd8d5bfe90211524dd8d0c58
lexaweb <59533947+lexaweb@users.noreply.github.com>
2020-07-28 15:33:21 +0300

    исправил табы

--- src/js/main.js
+++ src/js/main.js
@@ -1,60 +1,57 @@
 import $ from "jquery";
-import '@fancyapps/fancybox';
-import Inputmask from 'inputmask';
-import 'nouislider';
-import 'select2';
+import "@fancyapps/fancybox";
+import Inputmask from "inputmask";
+import "nouislider";
+import "select2";
 import Swiper from "swiper";
-import 'tooltipster';
+import "tooltipster";
 
 $(document).ready(() => {
-  $('select').select2();
+  $("select").select2();
 
-  var mySwiper = new Swiper('.swiper-container', {
+  var mySwiper = new Swiper(".swiper-container", {
     speed: 400,
     spaceBetween: 100,
     pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
+      el: ".swiper-pagination",
+      type: "bullets",
     },
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
     },
   });
 
-  $(".mb").click(function() {
+  $(".mb").click(function () {
     $(".header").toggleClass("active");
   });
 
+  $(".tab-content").each(function () {
+    $(this).find(".tab-content-item").not(":first").hide();
+  });
+  $(".tabs-hedings-title").click(function () {
+    $(this).addClass("active").siblings().removeClass("active");
+    $(this)
+      .parent()
+      .siblings(".tab-content")
+      .find(".tab-content-item")
+      .hide()
+      .eq($(this).index())
+      .fadeIn();
+    console.log($(".tab-content-item").index());
+  });
 
-  // $(function() {
+  var header = $(".header"),
+    scrollPrev = 0;
 
-  //   $('ul.tabs__caption').on('click', 'li:not(.active)', function() {
-  //     $(this).addClass('active').siblings().removeClass('active')
-  //       .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
-  //   })
-  
-  // })
-  
+  $(window).scroll(function () {
+    var scrolled = $(window).scrollTop();
 
-    $(".tab_item").not(":first").hide();
-    $(".tabs-img .tab").click(function() {
-      $(".tabs-img .tab").removeClass("active").eq($(this).index()).addClass("active");
-      $(".tab_item").hide().eq($(this).index()).fadeIn()
-    }).eq(0).addClass("active");
-
-    var header = $('.header'),
-		scrollPrev = 0;
-
-$(window).scroll(function() {
-	var scrolled = $(window).scrollTop();
- 
-	if ( scrolled > 50 && scrolled > scrollPrev ) {
-		header.addClass('hide');
-	} else {
-		header.removeClass('hide');
-	}
-	scrollPrev = scrolled;
-});
-  
+    if (scrolled > 50 && scrolled > scrollPrev) {
+      header.addClass("hide");
+    } else {
+      header.removeClass("hide");
+    }
+    scrollPrev = scrolled;
+  });
 });
